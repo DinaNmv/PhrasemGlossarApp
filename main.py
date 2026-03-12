982fcaa011914e328ba40bf3e863a58440f22329
Dina | App-Entwicklung <anidmin@gmail.com>
2026-03-12 16:09:10 +0100

    hamburger button

--- main.py
+++ main.py
@@ -12,6 +12,39 @@ import base64
 def get_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
+    
+
+# -------------------------------
+# Custom CSS für Hamburger Button
+# -------------------------------
+custom_css = """
+<style>
+
+/* Original Icon verstecken */
+button[data-testid="stBaseButton-headerNoPadding"] svg path {
+    display: none !important;
+}
+
+/* Eigenes Hamburger Icon */
+button[data-testid="stBaseButton-headerNoPadding"]::before {
+    content: "";
+    position: relative;
+    top: -6px;
+    left: 12px;
+    width: 20px;
+    height: 2px;
+    background: currentColor;
+    box-shadow:
+        0 6px currentColor,
+        0 12px currentColor;
+}
+
+</style>
+"""
+
+st.markdown(custom_css, unsafe_allow_html=True)
+
+
 
 # Anklickbares Phrasem (Button):
 st.markdown("""
